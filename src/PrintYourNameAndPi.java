4829fc8465fcf88df6d6aae983b83244e1c9e6a8
Mirko Franco <francomirko91@gmail.com>
2022-12-10 10:23:44 +0100

    ----------------

---
+++ src/PrintYourNameAndPi.java
@@ -0,0 +1,8 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name = "mirko";
+        System.out.printf("%S %n", name);
+        double number = 3.141590;
+        System.out.printf("The number Pi is: %f",number);
+    }
+}
